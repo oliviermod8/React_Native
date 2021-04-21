08ff546d1dc7e6a87e663e72e96572445c76abf1
oliviermod8 <oliviermod8@gmail.com>
2021-04-22 01:21:13 +0200

    debug + test

--- App.js
+++ App.js
@@ -1,33 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableWithoutFeedback, View, Image, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Universe !</Text>
-      <TouchableOpacity onPress={() => console.log('Image')}>
-      <Image style={{ width: 414, height: 780 }} source={require('./assets/system.jpg')}/>
-      </TouchableOpacity>
-      <Button title="Click Me" onPress={() => console.log('Button tap')} />
-     </View>
+      <Image source={require('./assets/mercury.jpeg')} style={styles.image}/>
+      <Image source={require('./assets/earth.jpeg')} style={styles.image}/>
+      <Image source={require('./assets/moon.jpeg')} style={styles.image}/>
+      <Image source={require('./assets/mars.jpeg')} style={styles.image}/>
+      <Image source={require('./assets/jupiter.jpeg')} style={styles.image}/>
+      <Text style={styles.text}>Hello universe !</Text>
+      <Image source={require('./assets/uranus.jpeg')} style={styles.image}/>
+      <Image source={require('./assets/neptune.jpeg')} style={styles.image}/>
+      {/* <Image source={require('./assets/sun.jpeg')} style={styles.image}/> */}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundPosition: "center center",
-    backgroundSize : "cover",
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-
+  image:{
+    height: 90,
+    width:90,
   },
   text: {
-    color: '#ffffff',
+    color: '#fff',
+    height: 120,
     fontSize:50,
+    display : 'flex',
     position : 'absolute'
   }
 });
