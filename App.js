ede9a7ed4e69583e28e20a7c3245980fdfb78041
oliviermod8 <oliviermod8@gmail.com>
2021-04-21 22:31:26 +0200

    1st local commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, View, Image, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.text}>Hello Universe !</Text>
+      <TouchableOpacity onPress={() => console.log('Image')}>
+      <Image style={{ width: 414, height: 780 }} source={require('./assets/system.jpg')}/>
+      </TouchableOpacity>
+      <Button title="Click Me" onPress={() => console.log('Button tap')} />
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#ffffff',
+    fontSize:50,
+    position : 'absolute'
+  }
 });
